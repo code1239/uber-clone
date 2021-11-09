f8ea73fa3eff2364bdf3d8a5cd2ead1ef94f1bb2
code1239 <yusufmacit98@gmail.com>
2021-11-09 19:37:52 +0300

    first commit 1

--- pages/confirm.js
+++ pages/confirm.js
@@ -44,7 +44,7 @@ const Confirm = () => {
     return (
         <Wrapper>
             <ButtonContainer>
-                <Link href="search">
+                <Link href="search" passHref={true}>
                 <BackButton src="https://img.icons8.com/ios-filled/50/000000/left.png" />
                 </Link>
             </ButtonContainer>
--- pages/search.js
+++ pages/search.js
@@ -11,7 +11,7 @@ const Search = () => {
     return (
         <Wrapper>
             <ButtonContainer>
-                <Link href="/">
+                <Link href="/" passHref={true}>
                 <BackButton src="https://img.icons8.com/ios-filled/50/000000/left.png" />
                 </Link>
             </ButtonContainer>
@@ -37,7 +37,7 @@ const Search = () => {
                     pickup: `${pickup}`, 
                     dropoff: `${dropoff}`
                 }
-            }}
+            }} passHref={true}
          
             > 
                 <ConfirmContainer>
